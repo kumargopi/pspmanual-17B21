7cf81cfbb781c01eb4cb16d323c85891a1ce744e
kumargopi <33897978+kumargopi@users.noreply.github.com>
2017-11-24 16:00:41 +0530

    Add files via upload

---
+++ n prime numbers.py
@@ -0,0 +1,15 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======================= RESTART: C:/Python27/nprime.py =======================
+>>> enter number: 15
+SyntaxError: invalid syntax
+>>> print 2
+2
+>>> print 14
+14
+>>> print 26
+26
+>>> print 27
+27
+>>> 
